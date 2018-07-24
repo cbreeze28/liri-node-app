5458f8368dd6c9ff478970a3e1ee993c07105da3
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-07-24 13:42:46 -0400

    added liri commacns

--- liri.js
+++ liri.js
@@ -4,11 +4,11 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require("./keys");
-
+var fs = require("fs");
 
 var client = new Twitter(keys.Twitter);
 
-var params = {screen_name: 'rbiswas'};
+var params = {screen_name: 'cbreeze28'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
@@ -28,3 +28,41 @@ spotify.search({ type: 'track', query: 'Hit me baby one more' }, function(err, d
 console.log(JSON.stringify(data,null, 2)); 
 });
 
+var source = process.argv[2];
+var input = "";
+
+userInput(source, input);
+
+function userInput(source, input) {
+  switch (source) {
+    case "my-tweets":
+    pullTweets();
+    break;
+
+    case "spotify-this-song":
+    var title = input;
+      if (title === "") {
+        findSong();
+      } else {
+        pullSong(title)
+      }
+      break;
+
+    case "movie-this":
+    var movie = input;
+      if (title === "") {
+        findMovie("The Rock");
+      } else {
+        pullMovie(movie)
+      }
+      break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+    break;
+  }
+}
+
+function pullTweets() {
+
+}
